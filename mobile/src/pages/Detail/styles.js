bed560ca54564a813d3938fb333e25f2f6f88bf9
henryjunior <henrybpj@live.com>
2020-03-29 21:23:38 -0300

    Mobile da aplicação

---
+++ mobile/src/pages/Detail/styles.js
@@ -0,0 +1,78 @@
+import { StyleSheet } from "react-native";
+import Constants from "expo-constants";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 20
+  },
+
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+
+  incident: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: "#FFF",
+    marginBottom: 16,
+    marginTop: 48
+  },
+
+  incidentProperty: {
+    fontSize: 14,
+    color: "#41414d",
+    marginTop: 24,
+    fontWeight: "bold"
+  },
+
+  incidentValue: {
+    marginTop: 8,
+    fontSize: 15,
+    color: "#737380"
+  },
+
+  contactBox: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: "#FFF",
+    marginBottom: 16
+  },
+
+  heroTitle: {
+    color: "#13131a",
+    fontSize: 20,
+    fontWeight: "bold",
+    lineHeight: 30
+  },
+
+  heroDescription: {
+    color: "#737380",
+    fontSize: 15,
+    marginTop: 16
+  },
+
+  actions: {
+    marginTop: 16,
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+
+  action: {
+    backgroundColor: "#e02041",
+    borderRadius: 8,
+    height: 50,
+    width: "48%",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  actionText: {
+    color: "#FFF",
+    fontSize: 15,
+    fontWeight: "bold"
+  }
+});
